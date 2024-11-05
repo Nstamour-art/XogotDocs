387d3b054121a5702ceb69b940d0e4010e9d0854
Joseph Hill <joseph@xibbon.com>
2024-11-05 18:23:57 -0500

    Initial Commit

---
+++ XogotDocs/XogotDocsApp.swift
@@ -0,0 +1,17 @@
+//
+//  XogotDocsApp.swift
+//  XogotDocs
+//
+//  Created by Joseph M Hill on 11/5/24.
+//
+
+import SwiftUI
+
+@main
+struct XogotDocsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
