ed45bf4e86516a85a36cb1ff0dfbd94d684e6622
Nicolas St-Amour <nstamour.dev@outlook.com>
2025-11-12 19:45:34 -0500

    Update HUD scripts to use 'Save the Axolotl!' label
    Replaces 'Dodge the Creeps!' with 'Save the Axolotl!' in the HUD scripts for consistency with the game's theme. Adds and improves comments for clarity, and refines the show_game_over logic to update the message label accordingly.

--- Documentation.docc/Resources/code-files/your-first-2d-game/hud-02.gd
+++ Documentation.docc/Resources/code-files/your-first-2d-game/hud-02.gd
@@ -3,6 +3,8 @@ extends CanvasLayer
 # Notifies `Main` node that the button has been pressed
 signal start_game
 
+
+# Updates, shows the message text and starts the timer
 func show_message(text):
 	$Message.text = text
 	$Message.show()
