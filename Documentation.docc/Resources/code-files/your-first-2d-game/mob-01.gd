5d8178d4a0ecac00aca33d3de2f4332fe59771ff
Nicolas St-Amour <nstamour.dev@outlook.com>
2025-11-16 12:51:29 -0500

    Add comments and tutorial image for mob signal
    Added explanatory comments to mob scripts for clarity and updated the tutorial to reference the correct code file. Introduced a new image illustrating mob signal connection for the 2D game tutorial.

--- Documentation.docc/Resources/code-files/your-first-2d-game/mob-01.gd
+++ Documentation.docc/Resources/code-files/your-first-2d-game/mob-01.gd
@@ -3,7 +3,8 @@ extends Area2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var mob_types = $AnimatedSprite2D.sprite_frames.get_animation_names()
+    # Get all the mob animation names as an array.
+    var mob_types = $AnimatedSprite2D.sprite_frames.get_animation_names()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
